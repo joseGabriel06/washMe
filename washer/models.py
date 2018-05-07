c6089f994caf3e8b639dd9b2d4d47a29102668b3
joseGabriel06 <joseguzmanlopez404@gmail.com>
2018-05-07 13:10:37 -0500

    formulario en español

--- washer/models.py
+++ washer/models.py
@@ -7,15 +7,15 @@ SEX_CHOICES = (
 )
 
 class Register(models.Model):
-	first_name = models.CharField(max_length=30)
-	last_name  = models.CharField(max_length=30)
-	emiil      = models.EmailField(max_length=70,blank=True)
-	phone      = models.CharField(max_length=30)
-	birthday   = models.DateTimeField()
+	first_name = models.CharField(max_length=30, verbose_name='Nombre')
+	last_name  = models.CharField(max_length=30, verbose_name='Apellidos' )
+	emiil      = models.EmailField(max_length=70,blank=True,verbose_name='Correo')
+	phone      = models.CharField(max_length=30, verbose_name='Celular' )
+	birthday   = models.DateTimeField(verbose_name='Fecha de nacimiento')
 	#image      = models.FileField(upload_to='image/%Y/%m/%d')
 	status 	   = models.BooleanField()
 	working    = models.BooleanField()
-	sex        = models.CharField(max_length=1,choices=SEX_CHOICES)
+	sex        = models.CharField(max_length=1,choices=SEX_CHOICES,verbose_name='Género')
 	timestamp  = models.DateTimeField(auto_now_add=True)
 	def __str__(self):
 		return self.first_name

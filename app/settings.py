274c2b6a1e4553278bcb88c0015be92a82905943
joseGabriel06 <joseguzmanlopez404@gmail.com>
2018-05-11 09:51:23 -0500

    debug

--- app/settings.py
+++ app/settings.py
@@ -147,8 +147,7 @@ ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window; you may, of course, us
 REGISTRATION_AUTO_LOGIN = True # Automatically log the user in.
 SITE_ID                 = 1
 LOGIN_REDIRECT_URL      ='pay'
-REGISTRATION_EMAIL_HTML = False
-LOGOUT_URL = 'home'
+LOGOUT_URL = 'homepage:home'
 #ACTIVATION_EMAIL_SUBJECT = 'dasf'
 
 SOCIAL_AUTH_TWITTER_KEY    = 'GZRLBBOz845n9awPAKgHTNbSa'
